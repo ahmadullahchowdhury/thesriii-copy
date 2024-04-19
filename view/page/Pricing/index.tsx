5a9fd35043d75760be6e6682db01cc6054f9884a
ahmadullahchowdhury <kowcher99@gmail.com>
2024-04-19 12:55:29 +0600

    pricig details modal

--- view/page/Pricing/index.tsx
+++ view/page/Pricing/index.tsx
@@ -1,10 +1,85 @@
+'use client';
 import { pricingData } from '@json-db';
-import React from 'react';
+import React, { useState } from 'react';
 import { IoIosArrowForward, IoIosPeople } from 'react-icons/io';
 import { ArrowLeft } from 'lucide-react';
 import Image from 'next/image';
+import ProjectModal from 'view/ui/shared-component/component/pricingModal';
 
 const PricingPage = () => {
+  const [open, setOpen] = useState(false);
+  const [selectedPricingDetails, setSelectedPricingDetails] = useState<number>(0);
+
+  const pricingDetails = [
+    {
+      title: 'Individual membership',
+      icon: '/icon/icon-1.png',
+      info: 'In case of questions, please contact us.',
+      price: '$99',
+      desc: `Membership is open to individuals who are currently involved in the services research, service innovation, and/or the service science field of interest either through their career and/or education.   Individual members have an opportunity to meet and discuss today’s current challenges, research projects, etc. with other like-minded services professionals from around the world.
+      `,
+      benefits: [
+        'Access to SRII member profiles',
+        'Participate in an SRII Special Interest Group (SIG)',
+        'Right to view, post and/or blog in the SRII online community',
+        'Receive the online e-newsletter',
+        'Access to the digital research library',
+        'Receive the member registration rate at the SRII annual conference and other associated events'
+      ]
+    },
+    {
+      title: 'Individual Partner membership',
+      icon: '/icon/icon-2.png',
+      price: '$49',
+      info: '*Annual fee for members is $49.00 US. You must list the name of a valid SRII partner to receive this membership level.',
+      desc: `Membership is open to individuals already belonging to SRII partner organizations like INFORMS, AMA, IEEE, for a 50% discount off of the membership fee of $49/yearly. Partner members have an opportunity to meet and discuss today’s current challenges, research projects, etc. with other like-minded services professionals from around the world.
+      `,
+      benefits: [
+        'Access to SRII member profiles',
+        'Participate in an SRII Special Interest Group (SIG)',
+        'Right to view, post and/or blog in the SRII online community',
+        'Receive the online e-newsletter',
+        'Access to the digital research library',
+        'Receive the member registration rate at the SRII annual conference and other associated events'
+      ]
+    },
+    {
+      title: 'Student membership',
+      icon: '/icon/icon-3.png',
+      info: 'In case of questions, please contact us.',
+      price: '$49',
+      desc: `Tailored for the college student that has an interest in pursuing services as a career. To be eligible for this membership, the individual must be currently registered as a full-or half time student in an academic program that has a services research, services innovation, and or a service science field of interest (undergraduate or graduate).
+      `,
+      benefits: [
+        'Access to SRII member profiles',
+        'Participate in an SRII Special Interest Group (SIG)',
+        'Right to view, post and/or blog in the SRII online community',
+        'Receive the online e-newsletter',
+        'Access to the digital research library',
+        'Receive the member registration rate at the SRII annual conference and other associated events'
+      ]
+    },
+    {
+      title: 'Institute membership',
+      info: 'If you are interested in learning more about how to become a Institute Member, please contact us',
+      icon: '/icon/icon-4.png',
+      price: 'There is a flat fee for institutes to register as a member.',
+      desc: `This membership is open to institutes from around the world who would like to help foster future collaboration between government, industry, institutes and universities around service research, service innovation and service science.`,
+      benefits: [
+        'A leadership role in one or more of the SRII Special Interest Groups (SIGs)',
+        'Consideration for a speaking role (keynote, breakout session, panelist, moderator) at one of the SRII annual events',
+        'Opportunity to publish an article and/or a paper in the SRII e-newsletter/digital research library',
+        'Unlimited membership for all institute employees at half the membership registration rate. This provides employees with access to:',
+        'Access to SRII member profiles',
+        'Participate in an SRII Special Interest Group (SIG)',
+        'Right to view, post and/or blog in the SRII online community',
+        'Receive the online e-newsletter',
+        'Access to the digital research library',
+        'Receive the member registration rate at the SRII annual conference and other associated events'
+      ]
+    }
+  ];
+
   return (
     <div className='bg-gradient-to-b from-neutral-100 to-indigo-300'>
       <div className=' min-h-screen bg-cover bg-[url(/image/Pricing.png)] pb-10 md:pb-20 lg:pb-28 '>
@@ -55,8 +130,16 @@ const PricingPage = () => {
                     </div>
                   </div>
                   <div className='pt-4 md:pt-5 lg:pt-6'></div>
-                  <div className='  px-7 py-[11px] bg-indigo-400 rounded-[5px] justify-center items-center gap-2.5 flex'>
-                    <div className='text-white text-base font-normal '>Details</div>
+                  <div className='cursor-pointer  px-7 py-[11px] bg-indigo-400 rounded-[5px] justify-center items-center gap-2.5 flex'>
+                    <div
+                      onClick={() => {
+                        setOpen(true);
+                        setSelectedPricingDetails(index);
+                      }}
+                      className='text-white text-base font-normal '
+                    >
+                      Details
+                    </div>
                   </div>
                 </div>
               </div>
@@ -64,6 +147,8 @@ const PricingPage = () => {
           ))}
         </div>
       </div>
+
+      <ProjectModal open={open} setOpen={setOpen} pricingDetails={pricingDetails[selectedPricingDetails]} />
     </div>
   );
 };
---
+++ view/ui/shared-component/component/pricingModal.tsx
@@ -0,0 +1,140 @@
+import { Fragment, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { RxCross2 } from 'react-icons/rx';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Image from 'next/image';
+
+// import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+
+export default function ProjectModal({
+  open,
+  setOpen,
+  pricingDetails
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  pricingDetails: {
+    title: string;
+    icon: string;
+    desc: string;
+    info: string;
+    price: string;
+    benefits: string[];
+  } | null;
+}) {
+  const cancelButtonRef = useRef(null);
+  console.log('pricingDetails', pricingDetails);
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as='div'
+        style={{ zIndex: 10000 }}
+        className='relative z-50'
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-transparent bg-opacity-50  ' />
+        </Transition.Child>
+
+        <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+          <div className='flex min-h-full  justify-center p-4 text-center items-center sm:p-0'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            >
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-2xl transition-all sm:my-8 sm:w-full sm:max-w-[741px]'>
+                <div className=' relative pt-5 px-8 sm:px-6 md:px-4  '>
+                  <div className=' flex  items-center justify-around sm:gap-4'>
+                    <div className='flex flex-col  md:flex-row gap-2'>
+                      <Image
+                        alt='all'
+                        width={119}
+                        height={119}
+                        className=''
+                        src={pricingDetails?.icon || '/default-icon.png'}
+                      />
+                    </div>
+                    <p className='text-[#5C6898] text-[22px] md:text-[32px] text-center lg:text-[25px] font-semibold  text-nowrap'>
+                      {pricingDetails?.title}
+                    </p>
+                    <div className=' cursor-pointer      ' onClick={() => setOpen(false)} ref={cancelButtonRef}>
+                      <RxCross2 className='h-8 w-8 text-gray-800' aria-hidden='true' />
+                    </div>
+                  </div>
+
+                  <div className='p-[8px] mt-[46px] '>
+                    <p className='text-[#5C6898] text-[14px] px-[80px]'>{pricingDetails?.desc}</p>
+                  </div>
+                  <p
+                    style={{
+                      borderRadius: '8px',
+                      background: 'rgba(239, 239, 239, 0.70)'
+                    }}
+                    className='px-10 text-[#5C6898] text-[14px] max-w-[280px] md:max-w-[643px] mx-auto text-center text-base py-[8px] mt-[30px]'
+                  >
+                    Benefits include
+                  </p>
+
+                  <ul className='mx-[70px] p-[8px] list-disc text-[#5C6898] '>
+                    {pricingDetails?.benefits.map((benefit, index) => (
+                      <li key={index} className='text-[#5C6898] text-[14px] '>
+                        {benefit}
+                      </li>
+                    ))}
+                  </ul>
+
+                  <p
+                    style={{
+                      borderRadius: '8px',
+                      background: 'rgba(239, 239, 239, 0.70)'
+                    }}
+                    className='px-10 text-[#5C6898] text-[14px] max-w-[280px] md:max-w-[643px] mx-auto text-center text-base py-[8px] mt-[40px]'
+                  >
+                    Membership fee
+                  </p>
+
+                  <div className='px-[60px] text-[16px] pt-[27px] space-y-[24px] text-[#5C6898]'>
+                    <p>
+                      {' '}
+                      Duration : <span className='font-semibold'> 1 year </span>{' '}
+                    </p>
+                    <p>
+                      {' '}
+                      Price : <span className='font-semibold'> {pricingDetails?.price} </span>{' '}
+                    </p>
+                    <p className='text-[#D24444] text-[10px] font-[500]'>{pricingDetails?.info}</p>
+                  </div>
+                  <div className='flex justify-end mt-[16px] mb-[67px] max-w-[643px] mx-auto '>
+                    <button
+                      type='button'
+                      className='flex justify-end w-full  rounded-md bg-[#6E8BEB] px-[28px] py-[11px] text-base font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto'
+                      // onClick={() => setOpen(false)}
+                    >
+                      Join
+                    </button>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
